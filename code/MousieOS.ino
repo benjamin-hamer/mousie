1ba23ebe81444990a07d2b91a1f72d568b4bd6ab
benjamin-hamer <thehamernator@gmail.com>
2022-01-06 15:14:28 +0000

    Set the modes of the pins

--- code/MousieOS.ino
+++ code/MousieOS.ino
@@ -10,3 +10,28 @@ const int rightEchoPin = 11;
 
 //select the pin for the IR sensor to communicate with
 int irpin = 2;
+
+//define variables for distance calculations
+long durationLeft;
+long durationFront;
+long durationRight;
+int distanceLeft;
+int distanceFront;
+int distanceRight;
+int a = 0;
+
+void setup(){
+  //set the trigger pins as outputs
+  pinMode(leftTrigPin, OUTPUT);
+  pinMode(frontTrigPin, OUTPUT);
+  pinMode(rightTrigPin, OUTPUT);
+  //set the echo pins as inputs
+  pinMode(leftEchoPin, INPUT);
+  pinMode(frontEchoPin, INPUT);
+  pinMode(rightEchoPin, INPUT);
+  //set the pins for the motor controller as outputs
+  pinMode(4, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
+}
