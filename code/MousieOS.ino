0c0f0177ce23774f386473825e7c79909dcf9f7c
benjamin-hamer <thehamernator@gmail.com>
2022-01-06 15:04:18 +0000

    Defined the pins for use in the robot

---
+++ code/MousieOS.ino
@@ -0,0 +1,12 @@
+//define the trigger pins for the ultrasonic sensors
+const int leftTrigPin = 3;
+const int frontTrigPin = 6;
+const int rightTrigPin = 10;
+
+//define the echo pins for the ultrasonic sensors
+const int leftEchoPin = 5;
+const int frontEchoPin = 9;
+const int rightEchoPin = 11;
+
+//select the pin for the IR sensor to communicate with
+int irpin = 2;
